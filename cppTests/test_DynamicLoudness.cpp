1b4589e34cf07b23b64ab6d294ce12aac2fc42ec
Jon Ford <jondavidford@gmail.com>
2014-11-09 15:06:59 -0600

    rewiting dynamic loudness test to verify values

--- cppTests/test_DynamicLoudness.cpp
+++ cppTests/test_DynamicLoudness.cpp
@@ -1,35 +1,34 @@
 // translated from ../pythonTests/test_DynamicLoudnessGM.py
-using namespace loudness;
-
 #include "../src/Models/DynamicLoudnessGM.h"
 
 int main()
 {
-	int fs = 32000;
+	int nChannels;
 	int hopSize = 32;
-	AudioFileCutter audio = AudioFileCutter("../wavs/tone1kHz40dBSPL.wav", hopSize);
+	loudness::AudioFileCutter audio = loudness::AudioFileCutter("../wavs/tone1kHz40dBSPL.wav", hopSize);
 	audio.initialize();
-	SignalBank audioBank = audio.getOutput();
-	int nFrames = audio.getNFrames();
+	loudness::SignalBank audioBank = audio.getOutput();
 
 	//Create the loudness model
-	DynamicLoudnessGM model = DynamicLoudnessGM("../filterCoefs/32000_IIR_23_freemid.npy");
+	loudness::DynamicLoudnessGM model = loudness::DynamicLoudnessGM("../filterCoefs/32000_IIR_23_freemid.npy");
 	model.initialize(audioBank);
-	SignalBank loudnessBank = model.getModuleOutput(model.getNModules()-1);
+	loudness::SignalBank loudnessBank = model.getModuleOutput(6);
 	nChannels = loudnessBank.getNChannels();
 
 	//storage
 	//out = np.zeros((nFrames, nChannels));
 
 	//processing
-	for (int frame = 0; frame < nFrames; frame++)
+	for (int frame = 0; frame < 5; frame++)
 	{
 	    audio.process();
 	    model.process(audioBank);
 
+	    cout << "frame: " << frame << endl << endl;
+
 	    for (int chn = 0; chn < nChannels; chn++)
 	    {
-	        //out[frame, chn] = loudnessBank.getSample(chn,0);
+	        cout << "bin: " << chn << "\t\tloudness: " << loudnessBank.getSample(chn,0);
 	    }
 	}
 }
